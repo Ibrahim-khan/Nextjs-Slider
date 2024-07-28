7d5d1192bab391183b0a3e9ff5f048269483a954
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-07-29 01:40:22 +0600

    Website-BioSpray-Nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Bio Spray Plus</h1>
+      <h1 className="text-3xl font-bold underline bg-red-200">
+        Bio Spray Plus
+      </h1>
     </main>
   );
 }
