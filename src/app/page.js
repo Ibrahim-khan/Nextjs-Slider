bc2cbf4a9031f24a169ddb812a303007c9b1cc07
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-08-02 01:14:30 +0600

    Website-BioSpray-Nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 import { Zoom } from 'react-slideshow-image'; 
 import 'react-slideshow-image/dist/styles.css';
+import "./slideshow.css"
 
 export default function Home() {
 
@@ -45,15 +46,22 @@ export default function Home() {
 
 
   return (
-    <main>
+    <main className="min-h-screen max-w-screen-2xl flex justify-center items-center position relative">
       <h1 className="text-3xl font-bold underline bg-red-200">
         Bio Spray Plus
       </h1>
-      <div className="">
-        <img src="coverphoto/coverphoto.png" />
-        
-            
+      
+      <div className="w-2/4 h-3/5 relative">
+        <Zoom {...zoomInPropertics}>
+          {images.map((each, index) => (
+            <div key={index} className="flex items-center justify-center h-full w-full">
+              <img className="w-full h-full rounded-lg" src={each}></img>
+            </div>
+          ))}
+
+        </Zoom>
       </div>
+
     </main>
   );
 
